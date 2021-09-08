4f5992f34c4dbf51f2f4ca199fe322fead46673f
Amoghsaraswat <75422601+Amoghsaraswat@users.noreply.github.com>
2021-09-08 16:54:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,16 @@
-noseX=0;
-noseY=0;
-difference=0;
-leftwristX=0;
-rightwristX=0;
+song=""
+function preload() {
+    song=loadSound("music.mp3");
+}
 function setup() {
-    video = createCapture(VIDEO);
-    video.size(550, 500);
-    canvas = createCanvas(400, 400);
-    canvas.position(560, 150);
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on("pose", gotPoses);
-}
-
-function modelLoaded() {
-    console.log("poseNet is Loaded");
-}
-
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+} 
 function draw() {
-    background("white");
-    document.getElementById("square").innerHTML="width and height of a square will be "+difference+"px";
-    fill("red");
-    stroke("red");
-    square(noseX,noseY,difference);
+    image(video,0,0,600,500);
 }
-
-function gotPoses(results) {
-    if (results.length > 0) {
-        console.log(results);
-       noseX=results[0].pose.nose.x;
-               noseY=results[0].pose.nose.y;
-        console.log("noseX = " + noseX +" noseY ="+noseY);
-        leftwristX=results[0].pose.leftWrist.x;
-        rightwristX=results[0].pose.rightWrist.x;
-        difference=floor(leftwristX-rightwristX);
-    
-    }
+function play() {
+    song.play();
 }
